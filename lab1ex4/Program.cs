04c30aa676851d4d4da72c60739cf64a1a553732
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:45:45 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+* Scrieti un program care va afisa semnul unui numar citit de la tastatura
+* - Daca numarul este pozitiv, va afisa "+"
+* - Daca numarul este negativ, va afisa "-"
+* - Daca numarul este 0, va afisa "0"
+*/
+
+using System.ComponentModel.Design;
+using System.Net.Security;
+
+Console.WriteLine("Va rugam introduceti numarul:");
+int numar = int.Parse(Console.ReadLine());
+
+if (numar > 0)
+{
+    Console.WriteLine("+");
+}
+else if (numar < 0)
+{
+    Console.WriteLine("-");
+}
+else
+{
+    Console.WriteLine("0");
+}
